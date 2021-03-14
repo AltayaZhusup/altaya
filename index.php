b84eb44f8bb79b56a6ea36762bad41a154c46650
AltayaZhusup <80045431+AltayaZhusup@users.noreply.github.com>
2021-03-15 03:12:31 +0600

    Update index.php

--- index.php
+++ index.php
@@ -9,6 +9,13 @@
 <h1>Altaya Zhusup</h1>
 <h2>Style Preferenses</h2>
 <img src="1.jpg">
-
+<td bgcolor="menthol">
+<h3>Menu</h3>
+<p>
+<a href="https://www.instagram.com/iamprincessbitches/">
+<img src="2.jpg">
+</p>
+<p>
+</td>
 </body>
 </html>
