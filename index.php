b63b63ccc8fe0d74006a706a8050878fad589ebf
AltayaZhusup <80045431+AltayaZhusup@users.noreply.github.com>
2021-03-17 17:30:59 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,13 @@
 <!DOCTYPE html>
 <html>
- <head>
-  <title>Altaya</title>
- <link rel="stylesheet" type="text/css" href="style.css">
- </head>
+<head>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link href="https://fonts.googleapis.com/css2?family=Kaushan+Script&display=swap" rel="stylesheet">">
+	<title>Altaya Zhusup</title>
+</head>
 <body>
- <p>
-  <a href="https://www.instagram.com/iamprincessbitches/"target="blank"><imf src="2.jpg""/></a>
 
-<h1>Altaya Zhusup</h1>
-<h2>Style Preferenses</h2>
-<img src="1.jpg">
-<td bgcolor="menthol">
-<h3>Menu</h3>
-<p>
-<a href="https://www.instagram.com/iamprincessbitches/">
-<img src="2.jpg">
-   down-sidebar {width:12;}
-</p>
-<p>
-</td>
+<h1>Altaya</h1>
+
 </body>
 </html>
