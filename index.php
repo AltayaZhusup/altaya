4e92fd38b839fafd12d9cb957f67f2cf718f9697
AltayaZhusup <80045431+AltayaZhusup@users.noreply.github.com>
2021-03-17 17:35:34 +0600

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<link rel="stylesheet" type="text/css" href="css/style.css">
-	<link href="https://fonts.googleapis.com/css2?family=Kaushan+Script&display=swap" rel="stylesheet">">
+	<link href="https://fonts.googleapis.com/css2?family=Kaushan+Script&display=swap" rel="stylesheet">
 	<title>Altaya Zhusup</title>
 </head>
 <body>
