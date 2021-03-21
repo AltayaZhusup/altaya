b6f1e2a64e8b9b18c1231b67dc5b8a32130e2b31
AltayaZhusup <80045431+AltayaZhusup@users.noreply.github.com>
2021-03-22 03:59:59 +0600

    Update index.php

--- index.php
+++ index.php
@@ -84,8 +84,6 @@
     <button type="submit" class="registerbtn">Register</button>
   </div>
 
-  <div class="container signin">
-    <p>Already have an account? <a href="#">Sign in</a>.</p>
   </div>
 </form>
 </body>
